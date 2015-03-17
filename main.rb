03e20afb425aae204c08c8605f9ff362b9ebbff3
Marta Baptista <martacpnbaptista@gmail.com>
2015-03-17 13:50:47 +0000

    connect to db

--- main.rb
+++ main.rb
@@ -1,9 +1,46 @@
 require 'sinatra'
 require 'json'
+require 'mysql'
 
 
+class MysqlCon
+ def initialize
+ 	begin
+ 		@connection = Mysql.new 'localhost', 'root', 'password2'
+	rescue Mysql::Error => e
+    	puts e.errno
+    	puts e.error    
+	end
+ end
+ def mysqlQuery (query)
+ 	result = @connection.query(query)
+ 	output = result.fetch_row
+ #	@connection.close if @connection
+ 	return output
+ end
+end
 
 set :public_folder, "./public"
+
+def init()
+	begin
+	    con = Mysql.new 'localhost', 'root', 'password2'
+	    puts con.get_server_info
+	    rs = con.query 'SELECT VERSION()'
+	    puts rs.fetch_row    
+	    
+	rescue Mysql::Error => e
+	    puts e.errno
+	    puts e.error
+	ensure
+	    con.close if con
+	end
+end
+
+mysqlhandler = MysqlCon.new()
+puts mysqlhandler.mysqlQuery('SELECT VERSION()')
+puts "hello"
+puts mysqlhandler.mysqlQuery('SELECT VERSION()')
 
 def get_modules_data()
   "hello".to_json
@@ -17,6 +54,7 @@ end
 
 
 get "/modules" do
+  
   content_type :json
   get_modules_data
 end

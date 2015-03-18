3884c7bb205126a095882061785ff978f86c0b8d
Fons <lord.fontana@gmail.com>
2015-03-18 09:57:16 +0100

    add json endpoint
    Signed-off-by: Fons <lord.fontana@gmail.com>

--- set_env.sh
+++
@@ -1,6 +0,0 @@
-
-#1) Edit the "password" field specifying the password of the database user
-#2) Run the command above
-#   source set_env.sh
-export  athens_user="root"
-export  athens_pass="password"

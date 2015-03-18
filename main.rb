c89870595869531868228eda6c3df35511c715d7
Marta Baptista <martacpnbaptista@gmail.com>
2015-03-18 18:17:16 +0000

    graph grades

--- main.rb
+++ main.rb
@@ -121,6 +121,11 @@ get '/g' do
   send_file File.join(settings.public_folder, 'grades.html')
 end
 
+get '/gradeslite' do
+ # erb :index,  :locals => {:data => data}
+  send_file File.join(settings.public_folder, 'gradeslite.html')
+end
+
 
 get "/semester" do
   node_name = params[:node]
@@ -148,6 +153,5 @@ get "/modulesnonoptional" do
   get_attendance_nonopt_data
 end
 get "/grades" do
-  content_type :json
   get_grades_data
 end

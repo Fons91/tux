a33b4bd3c4bb0f24051764cc4d1685a120af8291
Loudet Julien <loudet.julien@gmail.com>
2015-03-18 10:20:22 +0100

    Removed set_env.sh from shared files

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
 
 #1) Edit the "password" field specifying the password of the database user
-#2) Run the command above
+#2) Run the command below
 #   source set_env.sh
-export  athens_user="root"
-export  athens_pass="password"
+export  athens_user="tux"
+export  athens_pass="tux"
